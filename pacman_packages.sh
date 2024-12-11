798519ff0aa40a9a8b5d395d0e076cb05445e94f
Alexander Cieslewicz <alexander.cieslewicz@gmail.com>
2024-12-10 20:56:49 -0700

    Minimum new installs for working build

--- pacman_packages.sh
+++ pacman_packages.sh
@@ -4,20 +4,12 @@ echo ""
 echo "*** Getting needed packages ***"
 echo ""
 
-HOST=i686
+HOST=x86_64
 
-pacman -S mc git texinfo bison flex --noconfirm
-
-pacman -S mingw-w64-$HOST-binutils --noconfirm
-pacman -S mingw-w64-$HOST-gcc --noconfirm
-pacman -S mingw-w64-$HOST-clang --noconfirm
-pacman -S mingw-w64-$HOST-libtool --noconfirm
 pacman -S mingw-w64-$HOST-cmake --noconfirm
-pacman -S mingw-w64-$HOST-make --noconfirm
-pacman -S mingw-w64-$HOST-zlib --noconfirm
-pacman -S mingw-w64-$HOST-libxml2 --noconfirm
+pacman -S mingw-w64-$HOST-clang --noconfirm
+pacman -S make --noconfirm
 pacman -S mingw-w64-$HOST-dlfcn --noconfirm
-pacman -S mingw-w64-$HOST-autotools --noconfirm
 
 echo ""
 echo "*** All done ***"
